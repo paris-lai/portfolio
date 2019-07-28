65a75f0873a2c434c86a374b7a1e55a417752fd5
paris-lai <53387038+paris-lai@users.noreply.github.com>
2019-07-28 11:23:19 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleNavDropdown(){
-  let navToggle = document.querySelector('.topnav')
-  if(navToggle.className === 'topnav'){
-    navToggle.className += ' responsive';
-  }else{
-    navToggle.className = 'topnav';
-  }
-}
